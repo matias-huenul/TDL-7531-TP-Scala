f17728a3fad094cd9117d81dd99c4f316360b6c5
Martin Pata <mpata2000@fi.uba.ar>
2023-05-29 21:49:29 -0300

    Refactor of enum

--- src/main/scala/example/WebScrapper.scala
+++ src/main/scala/example/WebScrapper.scala
@@ -4,18 +4,30 @@ import org.json4s._
 import org.json4s.native.JsonMethods._
 import org.jsoup._
 
+import example.utils.Operation
 object WebScrapper{
 
   /*
-   * Operacion:
-   *    1 -> Venta
-   *    2 -> Alquiler
+   * @param stringData: String with json data
+   * @return List[JValue]: List of json objects
   */
-  def zonaprop(operacion:String = "2"): List[Propiedad] = {
+  def parseJson(stringData: String): List[JValue] ={
+    implicit val formats = DefaultFormats
+    val result = stringData.split("\\{\"listPostings\":")(1).split("\\,\"listCondominium\"")(0)
+    val json = parse(result)
+    json.extract[JArray].arr
+  }
+
+  /*
+   * @param operacion: String with the operation type ("1" = venta/ "2" alquiler)
+   * @return List[Propiedad]: List of properties
+  */
+  def zonaprop(operacion:Operation.Value = Operation.Alquiler): List[Propiedad] = {
     //Todo: add log
-    val operationStr = if (operacion == "1") "venta" else "alquiler"
+    implicit val formats = DefaultFormats
+
     val urlZonaProp = "https://www.zonaprop.com.ar"
-    val url = urlZonaProp + "/casas-departamentos-ph-" + operationStr + "-capital-federal"
+    val url = urlZonaProp + "/casas-departamentos-ph-" + operacion + "-capital-federal"
 
     var doc = Jsoup.connect(url + ".html")
       .userAgent("Mozilla")
@@ -26,38 +38,31 @@ object WebScrapper{
     val numberOfPages = (numberProperties/20)+1 // 20 propiedades por pagina
 
     // 461 seconds for 277 page y 5537 properties, 1.66 sec per page
-    for(i <- 1 to numberOfPages){
+    for(i <- 1 to 2){
       doc = Jsoup.connect(url + "-pagina-" + i + ".html")
         .userAgent("Mozilla")
         .get()
 
-      // Get json data from script
-      val s = doc.getElementById("preloadedData").data()
-      val result = s.split("\\{\"listPostings\":")(1).split("\\,\"listCondominium\"")(0)
-
-      implicit val formats = DefaultFormats
-
-      val json = parse(result)
-      val dataList = json.extract[JArray].arr
+      val dataList = parseJson(doc.getElementById("preloadedData").data())
 
       for(data <- dataList){
         val property = new Propiedad()
         property.id = (data \ "postingId").extract[String]
         property.url = urlZonaProp + (data \ "url").extract[String]
 
-        //
+        //Set data of price (precio, moneda, expensas)
         val operationTypes = (data \ "priceOperationTypes").extract[JArray].arr
         for (operationType <- operationTypes) {
-          if ((operationType \ "operationType" \ "operationTypeId").extract[String] == operacion) {
-            property.operacion = operationStr.capitalize
-            val prices = (operationType \ "prices")(0)
-            property.precio = (prices \ "amount").extractOpt[Int].getOrElse(0)
-            property.moneda = (prices \ "currency").extractOpt[String].getOrElse("ARS")
+          if ((operationType \ "operationType" \ "name").extract[String].compareToIgnoreCase(operacion.toString) == 0) {
+            property.operacion = operacion.toString.capitalize
+            val price = (operationType \ "prices")(0)
+            property.precio = (price \ "amount").extractOpt[Int].getOrElse(0)
+            property.moneda = (price \ "currency").extractOpt[String].getOrElse("ARS")
           }
         }
-
         property.expensas = (data \ "expenses" \ "amount").extractOpt[Int].getOrElse(0)
 
+        // Set data of features (superficie, ambientes, dormitorios, banios, cochera)
         val features = data \ "mainFeatures"
         val keys = features match {
           case JObject(fields) => fields.map { case (key, _) => key }
@@ -78,8 +83,10 @@ object WebScrapper{
           }
         }
 
+        // Set data of property type
         property.tipo = (data \ "realEstateType" \ "name").extract[String]
 
+        // Set data of location (barrio, direccion, coordenadas)
         val postLocation = data \ "postingLocation"
         property.direccion = (postLocation \ "address" \ "name").extractOpt[String].getOrElse("")
 
@@ -97,7 +104,7 @@ object WebScrapper{
         listProperties.append(property)
       }
     }
-
+    println(listProperties.head)
     listProperties.toList
   }
 }
---
+++ src/main/scala/example/utils/Operation.scala
@@ -0,0 +1,6 @@
+package example.utils
+
+object Operation extends Enumeration {
+  val Venta = Value("venta")
+  val Alquiler = Value("alquiler")
+}
